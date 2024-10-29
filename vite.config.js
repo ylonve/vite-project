245873759bb4b35922cf126d7cb001f26b7150bc
ylonve <90844955+ylonve@users.noreply.github.com>
2024-10-29 14:26:11 +0200

    adsf

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: 'https://github.com/ylonve/vite-project.git', // Replace 'your-repo-name' with the actual name of your GitHub repo
+});
